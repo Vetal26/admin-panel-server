a539b437aeb7d9fd82299c5ba7705a8978eee2e7
Vitaly Borisov <vitaliy.borisov@intspirit.com>
2021-12-12 17:45:46 +0300

    added models

---
+++ seeders/20211212141331-admin.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          firstName: 'Admin',
+          email: 'admin@mail.com',
+          password: 'admin',
+        },
+      ],
+      {},
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
